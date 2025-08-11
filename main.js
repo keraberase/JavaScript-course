2f3d8c65bef2786ff41f34920d9de31d344f5db9
keraberas <46618649+keraberase@users.noreply.github.com>
2025-08-11 20:29:46 +0100

    create project

---
+++ main.js
@@ -0,0 +1,14 @@
+// const addItem =  document.getElementById("quantityadd");
+// const removeItem =  document.getElementById("quantityremove");
+// const getElement = document.getElementById("quantity");
+
+
+// addItem.addEventListener('click', function() {
+// getElement.value = parseInt(getElement.value) +1;
+// });
+
+// removeItem.addEventListener('click', function() {
+//     if (parseInt(getElement.value) >0) {
+//     getElement.value = parseInt(getElement.value) -1;
+//     }
+// });
