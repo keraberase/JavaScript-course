6484446bab45042417c23b9cfcc0b15ea86e82f2
keraberas <46618649+keraberase@users.noreply.github.com>
2025-09-26 22:02:23 +0100

    create class

--- main.js
+++ main.js
@@ -12,3 +12,10 @@
 //     getElement.value = parseInt(getElement.value) -1;
 //     }
 // });
+const saleTaxRate = 0.75;
+incMsg = "This site under construction and will be fully functional soon.";
+function product(item,size,price){
+    this.item = item;
+    this.size = size;
+    this.price = price;
+}
