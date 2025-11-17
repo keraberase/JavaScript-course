6e1e47cbc697e888c62d0ed30abbb734e993023a
keraberas <46618649+keraberase@users.noreply.github.com>
2025-11-17 17:21:15 +0000

    add product mandolin and drum  finish task 23c

--- Session 2/Music2/Student/scripts/23c-main.js
+++ Session 2/Music2/Student/scripts/23c-main.js
@@ -1,4 +1,10 @@
 ﻿const salesTaxRate = .075;
 incMsg = "This site is under construction and will be fully functional soon.";
 
-// insert product class here
+class Product {
+    constructor(item, size, price) {
+        this.item = item;
+        this.size = size;
+        this.price = price;
+    }
+}
